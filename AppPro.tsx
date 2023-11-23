48ca298d7d39e8d95fccd30980b20aa89593944d
Ditipriya Sen <ditipriya.sen@illimitable.in>
2023-11-23 17:39:33 +0530

    complete

--- AppPro.tsx
+++ AppPro.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {View, Text, StyleSheet, useColorScheme} from 'react-native';
 
 function AppPro(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'light';
+  const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.container}>
-      <Text style={isDarkMode ? styles.greenText : styles.whiteText}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
         Hello World!
       </Text>
     </View>
@@ -15,6 +15,7 @@ function AppPro(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   whiteText: {
